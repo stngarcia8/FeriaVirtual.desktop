43bdaf72f6cb849cad9225f82337ff6e866251d1
Daniel.Garcia.Asathor <stnasathor@gmail.com>
2020-09-16 20:20:08 -0300

    Ajuste a la distribucion de las soluciones segun responsabilidad.

---
+++ api/FeriaVirtual.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FeriaVirtual.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ api/FeriaVirtual.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FeriaVirtual.API.WebApiApplication" Language="C#" %>
